10ae93b1bf65643457a35b3b53932087ba1eb615
bkapil002 <153111929+bkapil002@users.noreply.github.com>
2024-10-25 22:49:56 -0500

    Add files via upload

---
+++ string-2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    std:: string fristName = "Brahm";
+    std:: string lestName = "kapil";
+    std:: string fullname = fristName +  " " + lestName;
+    std:: cout << "Full Name: " << fullname << std :: endl;
+    return 0; 
+}
